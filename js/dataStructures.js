53f7682fc8a3c2f70379eb47ebcb6f121f664bac
John Grey <jgrey@soe.ucsc.edu>
2015-09-22 16:23:02 -0700

    broadly everything added

--- js/dataStructures.js
+++ js/dataStructures.js
@@ -38,15 +38,17 @@ define([],function(){
 
         //TODO: copy over bindings from parent,
         //then copy in new bindings
-        this.bindings = bindings | {};
+        this.bindings = bindings || {};
 
         this.id = startingID;
         startingId++;        
     }
 
     //Alpha Memory node
-    var AlphaMemory = function(){
+    var AlphaMemory = function(parent){
         this.items = [];
+        this.parent = parent;
+        this.parent.outputMemory = this;
         this.childNodes = [];
         this.referenceCount = 0;
         this.isMemoryNode = true;
@@ -65,9 +67,14 @@ define([],function(){
     
     //TODO:
     //A constant test node
-    var AlphaNode = function(constantTest){
+    var AlphaNode = function(parent,constantTest){
+        this.parent = parent;
+        this.parent.children.unshift(this);
+        this.outputMemory = undefined;
         this.children = [];
-        this.constantTest = constantTest;
+        this.testField = constantTest[0];
+        this.testValue = constantTest[1];
+        this.testOperator = constantTest[2] || 'EQ';
         this.id = startingId;
         startingId++;
     }
@@ -82,27 +89,40 @@ define([],function(){
     }
 
     //Beta Memory Stores tokens
-    var BetaMemory = function(){
-        ReteNode.call(this);
+    var BetaMemory = function(parent){
+        ReteNode.call(this,parent);
+        this.isBetaMemory = true;
         this.isMemoryNode = true;
+        this.dummy = dummy;
         this.items = [];
+        if(parent === undefined){
+            this.dummy = parent;
+            this.items.push(new Token());
+        }
         this.children = [];
     }
 
     //Join Node combines tokens with wmes
-    var JoinNode = function(alphaMemory,tests,ancestor){
-        ReteNode.call(this);
+    //tests are the binding tuples from a condition
+    var JoinNode = function(parent,alphaMemory,tests){
+        ReteNode.call(this,parent);
         this.type = "JoinNode";
+        this.isJoinNode = true;
         this.alphaMemory = alphaMemory;
         this.tests = tests;
-        this.ancestor = ancestor;
+        this.parent.children.unshift(this);
+        this.alphaMemory.children.unshift(this);
+        this.alphaMemory.referenceCount += 1;
+        this.nearestAncestor = null;
     }
 
     //ie:field1 = "first",field2 = "x"
     //used to compare bindings, stored in joinNodes
+    
     var TestAtJoinNode = function(field1,field2){
-        this.field1 = field1;//the name of the variable in wme
-        this.field2 = field2;//the bound name
+        this.wmeField = field1;//the name of the variable in wme
+        this.boundVar = field2;//the bound name
+        //TODO : this.operator
         this.id = startingId;
         startingId++;
     }
@@ -116,48 +136,119 @@ define([],function(){
     }
 
     //Negative Node:The node that gates token progression
-    var NegativeNode = function(alphaMemory,tests,ancestor){
-        ReteNode.call(this);
+    var NegativeNode = function(parent,alphaMemory,tests){
+        ReteNode.call(this,parent);
         this.isNegativeNode = true;
         this.items = [];
         this.alphaMemory = alphaMemory;
+        this.alphaMemory.referenceCount+;
         this.tests = tests;
-        this.ancestor = ancestor;
+        this.parent.children.unshift(this);
+        this.alphaMemory.children.unshift(this);
+        this.nearestAncestor = null;
     };
 
     //NCC : gates token progression based on a subnetwork
-    var NegatedConjunctiveCondition = function(partnerNode){
-        ReteNode.call(this);
+    var NegatedConjunctiveConditionNode = function(parent){
+        ReteNode.call(this,parent);
         this.isAnNCCNode = true;
         this.items = [];
-        this.partner = partnerNode;
+        this.partner = null;
+        this.parent.children.unshift(this);
     };
 
 
     //The partner of the NCC, connects to the subnetwork
-    var NegConjuConPartnerNode = function(nccNode){
+    var NegConjuConPartnerNode = function(parent,num){
+        ReteNode.call(this,parent);
         this.isAnNCCPartnerNode = true;
-        this.nccNode = nccNode;
-        this.numberOfConjuncts = 0;
+        this.nccNode = null;
+        this.numberOfConjuncts = num;
         this.newResultBuffer = [];
         this.id = startingId;
         this.type = "NCCPartnerNode";
-        startingId++;
+        this.parent.children.unshift(this);
+
     };
 
+    //Test: (wme.)a = 5
+    var Test = function(testWmeField,operator,testValue){
+        this.field = testWmeField;
+        this.operator = operator;
+        this.value = testValue;
+    };
+    
     //condition:
-    //tests: tuples (testField,testValue,comparisonFunction?)
-    //bindings: tuples (wmeVariableName,boundVariableName)
-    var Condition = function(tests,bindings){
-        this.constantTests = tests;
-        this.bindings = bindings;
+    //tests: triples (testField,operator,testValue)
+    //bindings: tuples (wmeVar,boundVar)
+    var Condition = function(tests,bindings,negative){
+        if(!negative){
+            this.isPositive = true;
+        }else{
+            this.isNegative = true;
+        }
+        this.constantTests = [];
+        this.bindings = [];
+        
+        for(var i in tests){
+            var v = tests[i];
+            var test = new Test(v[0],v[1],v[2]);
+            this.constantTests.push(test);
+        }
+        for(var i in bindings){
+            var v = bindings[i];
+            var binding = [v[0],v[1]];
+            this.bindings.push(binding);
+        }
+        this.bindings.sort(function(a,b){
+            if(a[0] < b[0]) return -1;
+            if(a[0] > b[0]) return 1;
+            return 0;
+        });
     };
 
+    var NCCCondition = function(conditions){
+        this.isNCCCondition = true;
+        this.conditions = [];
+        for(var i in conditions){
+            var cond = new Condition(conditions[i][0],conditions[i][1],conditions[i][2]);
+            this.conditions.push(cond);
+        }
+    }
+    
     //The rule/production that stores conditions and
     //associated action
-    var Rule = function(name,action,conditions){
-
+    var Rule = function(name,conditions,action){
+        this.name = name;
+        this.action = action;
+        this.conditions = [];
+        for(var i in conditions){
+            //[[tests+], [bindings+], ncc?]
+            var v = conditions[i];
+            if(v.length > 1){
+                var cond = new Condition(conditions[i][0],conditions[i][1],conditions[i][2]);
+            }else{
+                //NCC conditions are just wrapped in an array
+                var cond = new NCCCondition(conditions[0][0]);
+            }
+            this.conditions.push(cond);
+        }
     };
+
+    var ActionNode = function(parent,action,name){
+        ReteNode.call(this,parent);
+        this.isActionNode = true;
+        this.name = name;
+        this.action = action;
+        this.parent.children.unshift(this);
+    };
+
+    var ReteNet = function(){
+        this.dummyBetaMemory = new BetaMemory();
+        this.rootAlpha = new AlphaNode();
+        this.actions = [];
+    };
+
     
     //--------------------
     //DataStructures interface
--- js/procedures.js
+++ js/procedures.js
@@ -26,11 +26,13 @@ define(['./dataStructures'],function(DataStructures){
 
         //if true, activate children
     };
+
     
     //TODO:
     //Assert a wme into the network
     var addWME = function(wme,reteNet){
         //activate the root node of the reteNet
+        alphaNodeActivation(reteNet.rootAlpha,wme);
     };
     
     //trigger a beta memory to store a new token
@@ -55,10 +57,11 @@ define(['./dataStructures'],function(DataStructures){
         //TODO:copy existing bindings from token
         for(var i in joinNode.tests){
             var test = joinNode.tests[i];
-            var wmeValue = wme.data[test.field1];
+            var wmeValue = wme.data[test.wmeField];
             //If the binding exists
             if(token.bindings[test.field2]){
-                var tokenValue = token.bindings[test.field2];
+                var tokenValue = token.bindings[test.boundVar];
+                //TODO:Add Operators here
                 if(wmeValue !== tokenValue){
                     //binding doesnt match, break.
                     return false;
@@ -105,12 +108,11 @@ define(['./dataStructures'],function(DataStructures){
     //pulling all necessary tokens from the parent as needed
     var joinNodeRightActivation = function(node,wme){
         //relink or unlink as necessary
-        if(node.alphaMemory.childNodes.length === 0){
+        if(node.alphaMemory.childNodes.length === 1){
             relinkToBetaMemory(node);
             if(node.parent.items.length === 0){
-                node.alphaMemory.childNodes = node.alphaMemory.childNodes.filter(function(d){
-                    return d.id !== node.id;
-                });
+                var index = node.alphaMemory.children.map(function(d){ return d.id; }).indexOf(node.id);
+                node.alphaMemory.children.splice(index,1);
             }
         }
 
@@ -131,13 +133,15 @@ define(['./dataStructures'],function(DataStructures){
     //reconnect an unlinked join node to its alpha memory when there are
     //wmes in said alpha memory
     var relinkToAlphaMemory = function(node){
-        var ancestor = node.ancestor;
-        while(ancestor && ancestor.ancestor){
-            ancestor = ancestor.ancestor;
+        var ancestor = node.nearestAncestor;
+        var indices = node.alphaMemory.children.map(function(d){ return d.id; });
+        
+        while(ancestor && indices.indexOf(ancestor.id) === -1{
+            ancestor = findNearestAncestorWithSameAlphaMemory(ancestor,node.alphaMemory.id);
         }
-        if(ancestor){
-            //TODO: insert node in node.amem.children
-            //prior to ancestor
+        if(ancestor !== null){
+            var index = node.alphaMemory.children.map(function(d){ return d.id; }).indexOf(ancestor.id);
+            node.alphaMemory.children.splice(index,0,node);
         }else{
             node.alphaMemory.children.push(node);
         }
@@ -152,9 +156,9 @@ define(['./dataStructures'],function(DataStructures){
     //Trigger a negative node from a new token
     //brings in bindings, creates a new token as necessary,
     //combining bindings to.
-    var negativeNodeLeftActivation = function(node,token,wme,bindings){
-        if(node.items.length === 0){
-            relinkToalphaMemory(node);
+    var negativeNodeLeftActivation = function(node,token,bindings,wme){
+        if(node.items.length === 1){
+            relinkToAlphaMemory(node);
         }
         var newToken = new DataStructures.Token(token,wme,node,bindings);
         node.items.unshift(newToken);
@@ -199,7 +203,7 @@ define(['./dataStructures'],function(DataStructures){
     };
 
     //from a new token, trigger the subnetwork?
-    var nccNodeLeftActivation = function(nccNode,token,wme,bidings){
+    var nccNodeLeftActivation = function(nccNode,token,bidings,wme){
         //Create and store the incoming token from prior join node
         var newToken = new DataStructures.Token(token,wme,nccNode,bindings);
         nccNode.items.unshift(newToken);
@@ -225,7 +229,7 @@ define(['./dataStructures'],function(DataStructures){
 
     //the nccPartnerNode is activated by a new token from the subnetwork
     //figure out who owns this new token from the main (positive) network
-    var nccParterNodeLeftActivation = function(partner,token,wme,bindings){
+    var nccParterNodeLeftActivation = function(partner,token,bindings,wme){
         var nccNode = partner.nccNode;
         var newToken = new DataStructures.Token(token,wme,partner,bindings);
         
@@ -377,131 +381,335 @@ define(['./dataStructures'],function(DataStructures){
     //--------------------
     // Creation of Network:
     //--------------------
+
+    var compareNodeToTest = function(node,testTuple){
+        if(node.testField !== testTuple[0]
+           || node.testValue !== testTuple[1]){
+            return false;
+        }
+        if(testTuple.length === 3
+           && node.testOperator !== testTuple[2]){
+            return false;
+        }
+        return true;
+    };
     
-    //added parameter of 'retenet' for hash table lookup
-    var buildOrShareAlphaMemory = function(condition,reteNet){
+    var buildOrShareConstantTestNode = function(parent,testTuple){
+        for(var i in parent.children){
+            var node = parent.children[i];
+            if(compareNodeToTest(node,testTuple)){
+                return node;
+            }
+        }
+        var newAlphaNode = new AlphaNode(parent,testTuple);
+        return newAlphaNode;
+    }
+
+    
+    //added retenet parameter
+    //Remind: Rule{Conditions:Condition{constantTests
+    var buildOrShareAlphaMemory = function(condition,root){
+        var currentNode = root;
+        for(var i in condition.constantTests){
+            var constantTestTuple = condition.constantTests[i];
+            currentNode = buildOrShareConstantTestNode(currentNode,constantTestTuple);
+        }
         //see if there is an existing memory for this condition. if so, return existing alphamemory
-
+        if(currentNode.outputMemory !== undefined){
+            return currentNode.outputMemory;
+        }
         //else: create the alpha memory
-
+        var newAlphaMemory = new AlphaMemory(currentNode);
         //run wmes in working memory against the alpha network
-
-        return alphamemory
+        return newAlphaMemory;
     };
 
-    var buildOrShareBetaMemoryNode = function(node){
+    var buildOrShareBetaMemoryNode = function(parent){
         //if theres an available beta memory to use,
         //return that
-
+        for(var i in parent.children){
+            var child = parent.children[i];
+            if(child.isBetaMemory){
+                return child;
+            }
+        }
         //else: create a new beta memory
-        //update it with matches
+        var newBetaMemory = new BetaMemory(parent);
+        //TODO:update it with matches
         //return new beta memory
+        return newBetaMemory;
     };
 
     //Return the bindings from a condition
-    var getJoinTestsFromconditions(condition){
-
+    //a : first_field_of_wme
+    //Just tuples of (boundVariable,wmeField)
+    //To be entries in the token's binding dictionary
+    var getJoinTestsFromCondition(condition){
+        return condition.bindings;
     }
 
     //walk up from a beta node until you find a node
     //connected to the specified alpha memory
     var findNearestAncestorWithSameAlphaMemory(node,alphaMemory){
-
+        if(node.dummy){ return null;}
+        if(node.isJoinNode || node.isNegativeNode){
+            if(node.alphaMemory.id === alphaMemory.id){
+                return node;
+            }
+        }
+        if(node.isAnNCCNode){
+            return findNearestAncestorWithSameAlphaMemory(node.partner.parent,alphaMemory);
+        }
+        return findNearestAncestorWithSameAlphaMemory(node.parent,alphaMemory);        
     };
 
+    var compareJoinTests = function(firstTestSet,secondTestSet){
+        var i = firstTestSet.length;
+        var j = secondTestSet.length;
+        while(i || j){
+            if(firstTestSet[i][0] === secondTestSet[j][0]
+               && firstTestSet[i][1] === secondTestSet[j][1]){
+                i--; j--;
+            }else if(firstTestSet[i][0] > secondTestSet[j][0]){
+                i--;
+            }else{
+                j--;
+            }
+        }
+        if(i === j && i === 0){        
+            return true;
+        }
+        return false;
+    };
+    
 
     var buildOrShareJoinNode = function(parent,alphaMemory,tests){
         //see if theres a join node to use already
-        //return it
-
+        for(var i in parent.children){
+            var child = parent.children[i];
+            if(child.isJoinNode && child.alphaMemory.id === alphaMemory.id && compareTests(child.tests,tests)){
+                //return it
+                return child;
+            }
+        }
         //else: create a new join node
-        //increment alphamemories reference count
-        //if either parent memory is empty, unlink
+        //increment alphamemories reference count in the constructor
+        var newJoinNode = new JoinNode(parent,alphaMemory,tests);
+        set the nearest ancestor
+        newJoinNode.nearestAncestor = nearestAncestorWithSameAlphaMemory(parent,alphaMemory);
 
+        //if either parent memory is empty, unlink
+        if(parent.items.length === 0){
+            var index = alphaMemory.children.map(function(d){ return d.id; }).indexOf(newJoinNode.id);
+            alphaMemory.children.splice(index,1);
+        }else if(alphaMemory.items.length === 0){
+            var index = parent.children.map(function(d){
+                return d.id;
+            }).indexOf(newJoinNode.id);
+            parent.children.splice(index,1);
+        }
         //return new join node
-        
+        return newJoinNode;
     };
     
     var buildOrShareNegativeNode = function(parent,alphaMemory,tests){
         //see if theres an existing negative node to use
-        //return it
-
-        //else: create negative node
-        //increment alphamemory reference count
+        for(var i in parent.children){
+            var child = parent.children[i];
+            if(child.isNegativeNode
+               && child.alphaMemory.id === alphaMemory.id
+               && compareTests(child.tests,tests)){
+                return child;
+            }
+        }
+        var newNegativeNode = new NegativeNode(parent,alphaMemory,tests);
+        newNegativeNode.nearestAncestor = nearestAncestorWithSameAlphaMemory(parent,alphaMemory);
         //update with matches
         //unlink if it has no tokens
+        if(newNegativeNode.items.length === 0){
+            var index = alphaMemory.children.map(function(d){
+                return d.id;
+            }).indexOf(newNegativeNode.id);
+            alphaMemory.children.splice(index,1);
+        }
         //return new negative node
+        return newNegativeNode
     };
 
 
-    var buildOrShareNCCNodes = function(parent,conditions){
+    var buildOrShareNCCNodes = function(parent,conditions,rootAlpha){
         //build a network for the conditions
-
+        var bottomOfSubNetwork = buildOrShareNetworkForConditions(parent,conditions,rootAlpha);
         //find an existing NCCNode with partner to use
-        //return it
-
+        for(var i in parent.children){
+            var child = parent.children[i];
+            if(child.isAnNCCNode && child.partner.parent.id === bottomOfSubNetwork.id){
+                return child;
+            }
+        }
         //else: build NCC and Partner nodes
+        var newNCC = new NegatedConjunctiveConditionNode(parent);
+        var newNCCPartner = new NegConjuConPartnerNode(bottomOfSubNetwork,conditions.length);
+        newNCC.partner = newNCCPartner;
+        newNCCPartner.nccNode = newNCC;
         //update NCC
         //update partner
-        
-        
+        return newNCC;
     };
 
 
-    var buildOrShareNetworkForConditions = function(parent,conditions,earlierConditions){
+    var buildOrShareNetworkForConditions = function(parent,conditions,rootAlpha){
+        var currentNode = parent;
         //for each condition
-        //if condition is positive:
-        //build betamemory
-        //build alphamemory
-        //build join node
-        
-        //if condition is negative:
-        //build alpha memory
-        //build negative node
-                
-        //if condition is NCC:
-        //build ncc
-        
-        //return current node        
+        for(var i in conditions){
+            var condition = conditions[i];
+            if(condition.isPositive){
+                currentNode = buildOrShareBetaMemoryNode(currentNode);
+                var tests = getJoinTestsFromCondition(condition);
+                var alphaMemory = buildOrShareAlphaMemory(condition,rootAlpha);
+                currentNode = buildOrShareJoinNode(currentNode,alphaMemory,condition.bindings);
+            }else if(condition.isNegative){
+                var tests = getJoinTestsFromCondition(condition);
+                var alphaMemory = buildOrShareAlphaMemory(condition,rootAlpha);
+                currentNode = buildOrShareNegativeNode(currentNode,alphaMemory,tests);
+            }else if(condition.isNCCCondition){
+                currentNode = buildOrShareNCCNodes(currentNode,condition,rootAlpha);
+            }else{
+                throw new Error("Unrecognised condition type");
+            }
+            //return current node
+            return currentNode;
+        }
     };
-    
 
-    var addRule = function(conditions){
+    var activateActionNode = function(actionNode,token){
+        throw new Error("not implemented yet");
+    };
+
+    
+    var addRule = function(rule,reteNet){
         //build network with a dummy node for the parent
+        var currentNode = buildOrShareNetworkForConditions(reteNet.dummyBetaMemory,rule.conditions,reteNet.rootAlpha);
         //build new action node
+        var actionNode = new ActionNode(currentNode,rule.action,rule.name);
         //update node with matches
+        reteNet.actions[actionNode.name] = actionNode;
+        return actionNode;
     }
 
+    var leftActivate = function(node,token,joinTestResults,wme){
+        if(node.isBetaMemory){
+            betaMemoryLeftActivation(node,token,joinTestResults);
+        }else if(node.isJoinNode){
+            joinNodeLeftActivation(node,token);
+        }else if(node.isNegativeNode){
+            negativeNodeLeftActivation(node,token,bindings,wme);
+        }else if(node.isAnNCCNode){
+            nccNodeLeftActivation(node,token,bindings,wme);
+        }else if(node.isAnNCCPartnerNode){
+            nccPartnerNodeLeftActivation(node,token,bindings,wme);
+        }else if(node.isActionNode){
+            activateActionNode(node,token);
+        }
+        
+    };
+
+    var rightActivate = function(node,wme){
+        if(node.isJoinNode){
+            joinNodeRightActivation(node,wme);
+        }else if(node.isNegativeNode){
+            negativeNodeRightActivation(node,wme);
+        }else{
+            throw new Error("Tried to rightActivate Unrecognised node");
+        }
+    };
+        
     var updateNewNodeWithMatchesFromAbove = function(newNode){
-        //if parent is:
-        //a beta memory
-        //left activate
-        
-        //a join node
-        //left activate only the new node
-        
-        //a negative node
-        //left activate
-        
-        //an ncc
-        //left activate
+        var parent = newNode.parent;
+        if(parent.isBetaMemory){
+            for(var i in parent.items){
+                leftActivate(newNode,parent.items[i]);
+            }
+        }else if(parent.isJoinNode){
+            var savedChildren = parent.children;
+            parent.children = [newNode];
+            for(var i in parent.alphaMemory.items){
+                var item = parent.alphaMemory.items[i];
+                rightActivate(parent,item.wme);
+            }
+            parent.children = savedChildren;
+            }else if(parent.isNegativeNode){
+                for(var i in parent.items){
+                    var token = parent.items;
+                    if(token.negJoinResults.length === 0){
+                        leftActivate(newNode,token);
+                    }
+                }
+        }else if(parent.isAnNCCNode){
+            for(var i in parent.items){
+                var token = parent.items;
+                if(token.nccResults.length === 0){
+                    leftActivate(newNode,token);
+                }
+            }
+        }
     };
 
 
-    var removeRule = function(rule){
+    var removeRule = function(actionNode){
         //delete from bottom up
+        deleteNodeAndAnyUnusedAncestors(
     };
 
     var deleteNodeAndAnyUnusedAncestors = function(node){
         //if NCC, delete partner to
-
+        if(node.isAnNCCNode){
+            deleteNodeAnAnyUnusedAncestors(node.partner);
+        }
+        
         //clean up tokens
+        if(node.isBetaMemory){
+            while(node.items.length > 0){
+                deleteTokenAndDescendents(node.items[0]);
+            }
+        }
+        if(node.isAnNCCPartnerNode){
+            while(node.newResultBuffer.length > 0){
+                deleteTokenAndDescendents(node.items[0]);
+            }
+        }
 
         //clean up alphamemory
-
-        //clean up parent
+        if(node.isJoinNode || node.isNegativeNode){
+            var index = node.alphaMemory.children.map(function(d){ return d.id; }).indexOf(node.id);
+            if(index > -1){
+                node.alphaMemory.children.splice(index,1);
+            }
+            node.alphaMemory.referenceCount--;
+            if(node.alphaMemory.referenceCount === 0){
+                deleteAlphaMemory(node.alphaMemory);
+            }
+        }
         
-
+        //clean up parent
+        if(node.alphaMemory){
+            var index = node.parent.children.map(function(d){ return d.id; }).indexOf(node.id);
+            if(index > -1){
+                node.parent.children.splice(index,1);
+            }
+        }
+        if(node.isJoinNode){
+            var index = node.parent.children.map(function(d){
+                return d.id;
+            }).indexOf(node.id);
+            node.parent.children.splice(index,1);
+            if(node.parent.allChildren.length === 0){
+                deleteNodeAndAnyUnusedAncestors(node.parent);
+            }
+        }else if(node.parent.children.length === 0){
+            deleteNodeAndAnyUnusedAncestors(node.parent);
+        }
+        //deallocate memory for none
     };
     
 });
